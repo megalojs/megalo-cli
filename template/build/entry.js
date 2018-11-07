8175d329c9a818a45fab04c8f3ecb5d1f7801e56
Gitea <gitea@fake.local>
2018-11-07 16:53:13 +0800

    fixed subpacakge directory generation issue.

--- template/build/entry.js
+++ template/build/entry.js
@@ -1,22 +1,36 @@
 'use strict'
 const glob = require('glob')
-const { resolve } = require('./util')
+const path = require('path')
 
 // 获取指定目录下符合glob的所有文件
-function getEntry(globPath) {
-    var files = glob.sync(globPath);
+function getEntry(...globPath) {
+    var files = [];
+    globPath.forEach((path) => {
+        var pages = glob.sync(path)
+        pages.forEach((page) => {
+            files.push(page)
+        })
+    })
+
     var entries = {},
         pageName;
 
     files.forEach((entry) => {
-        pageName = entry.match(/\/(pages\/[^\/.]*\/index)\.js$/)[1];
-        entries[pageName] = entry;
+        pageName = entry.match(/([^\/.]*\/pages\/[^\/.]*\/index)\.js$/)[1];
+        if (pageName.startsWith('src')) {
+            pageName = pageName.replace('src/', '')
+        }
+        entries[pageName] = path.resolve(entry);
     })
     return entries;
 }
 
-module.exports = (()=> {
+module.exports = (() => {
     let entry = {}
-    entry = getEntry(resolve('src/pages/*/index.js'));
+    let paths = [
+        path.resolve(__dirname, '../src/pages/*/index.js'),
+        path.resolve(__dirname, '../src/*/pages/*/index.js')
+    ]
+    entry = getEntry(...paths);
     return entry
 })()
