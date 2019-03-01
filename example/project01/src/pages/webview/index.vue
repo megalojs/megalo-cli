f93b402506c02066123ca1474bca2c30fa714714
bigMeow <running@vip.163.com>
2019-03-01 19:37:03 +0800

    demo

---
+++ example/project01/src/pages/webview/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">
+    <web-view :src="url"></web-view>
+  </div>
+</template>
+
+<script>
+
+const urls = {
+  alipay: 'https://render.alipay.com/p/s/web-view/index',
+  swan: 'https://baidu.com/',
+  wechat: 'https://mp.weixin.qq.com/'
+}
+
+export default {
+  data () {
+    return {
+      url: ''
+    }
+  },
+  created () {
+    const { platform } = this.$mp
+    this.url = urls[platform]
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
