9196efbc7d2b23c08a60c683a68eef4b686bbfee
bigMeow <running@vip.163.com>
2019-03-04 16:07:49 +0800

    add system env whilte list

--- packages/@megalo/cli-service/lib/utils/resolveClientEnv.js
+++ packages/@megalo/cli-service/lib/utils/resolveClientEnv.js
@@ -1,9 +1,10 @@
 const prefixRE = /^VUE_APP_/
+const systemEnvWhiteList = ['NODE_ENV', 'PLATFORM']
 
 module.exports = function resolveClientEnv (raw = false) {
   const env = {}
   Object.keys(process.env).forEach(key => {
-    if (prefixRE.test(key) || key === 'NODE_ENV') {
+    if (prefixRE.test(key) || systemEnvWhiteList.includes(key)) {
       env[key] = process.env[key]
     }
   })
