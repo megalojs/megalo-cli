edc6d8c146fb31a2226fe7f2d099364e01f3c7b4
bigMeow <running@vip.163.com>
2019-03-06 11:24:51 +0800

    feat: add env for template

--- packages/@megalo/cli/template/src/App.vue
+++ packages/@megalo/cli/template/src/App.vue
@@ -2,23 +2,23 @@
 <script>
   export default {
     mpType: 'app',
-    onLaunch: function(options) {
+    onLaunch: function (options) {
       // Do something initial when launch.
-      console.log('App onLaunch')
+      console.log('App onLaunch, env文件自定义的环境变量VUE_APP_TEST值:', process.env.VUE_APP_TEST)
     },
-    onShow: function(options) {
+    onShow: function (options) {
       // Do something when show.
       console.log('App onShow')
     },
-    onHide: function() {
+    onHide: function () {
       // Do something when hide.
       console.log('App onHide')
     },
-    onError: function(msg) {
+    onError: function (msg) {
       console.log('App onError')
       console.log(msg)
     },
-    globalData() {
+    globalData () {
       return {
         a: 'I am global data'
       }
