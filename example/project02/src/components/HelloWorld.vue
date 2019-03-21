bd6b3ffe8015bd2ba7603f49d20a7345e3ccaab3
bigMeow <running@vip.163.com>
2019-03-22 01:32:27 +0800

    add: typescript demo

---
+++ example/project02/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1 :style="'color:'+color">{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component } from 'vue-property-decorator'
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop(String) color
+  @Prop({ default: 'Welcome to Megalo' }) msg
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hello{
+    font-size: 16px;
+    text-align: center;
+  }
+</style>
---
+++ example/project02/src/index.js
@@ -0,0 +1,58 @@
+import App from './App'
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import VHtmlPlugin from '@megalo/vhtml-plugin'
+
+Vue.use(VHtmlPlugin)
+
+// https://developers.weixin.qq.com/miniprogram/dev/framework/app-service/page.html
+Component.registerHooks([
+  'onLoad', // 监听页面加载
+  'onShow', // 监听页面显示
+  'onReady', // 监听页面初次渲染完成
+  'onHide', // 监听页面隐藏
+  'onUnload', // 监听页面卸载
+  'onPullDownRefresh', // 监听用户下拉动作
+  'onReachBottom', // 页面上拉触底事件的处理函数
+  'onShareAppMessage', // 用户点击右上角分享
+  'onPageScroll', // 页面滚动
+  'onTabItemTap' // 当前是 tab 页时， 点击 tab 时触发
+])
+
+const app = new Vue(App)
+
+app.$mount()
+
+export default {
+  config: {
+    // pages 的首个页面会被编译成首页
+    pages: [
+      'pages/hello',
+      'pages/my/my'
+    ],
+    tabBar: {
+      color: '#333',
+      selectedColor: '#007d37',
+      list: [
+        {
+          pagePath: 'pages/hello',
+          text: 'home',
+          iconPath: 'native/tabbar/home.png',
+          selectedIconPath: 'native/tabbar/home_on.png'
+        },
+        {
+          pagePath: 'pages/my/my',
+          text: 'my',
+          iconPath: 'native/tabbar/mine.png',
+          selectedIconPath: 'native/tabbar/mine_on.png'
+        }
+      ]
+    },
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'Typescript',
+      navigationBarTextStyle: 'black'
+    }
+  }
+}
