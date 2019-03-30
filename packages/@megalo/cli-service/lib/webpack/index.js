526b7d55409e43ba0224b77a57650e01fad86a97
bigMeow <running@vip.163.com>
2019-03-30 11:06:11 +0800

    fix: use 'fs-extra' instead 'rimraf'

--- packages/@megalo/cli-service/lib/webpack/index.js
+++ packages/@megalo/cli-service/lib/webpack/index.js
@@ -1,18 +1,15 @@
-const romoveFile = require('rimraf')
+const fs = require('fs-extra')
 const path = require('path')
 const webpack = require('webpack')
 const { mergeUserConfig } = require('./mergeUserConfig')
 
-module.exports = (...args) => {
+module.exports = async (...args) => {
   const commandName = args[0]
   const projectOptions = args[2]
 
   const chainConfig = (process.env.PLATFORM === 'h5' ? require(`./h5/${commandName}`) : require(`./mp/${commandName}`))(...args) || {}
   const finalWebpackConfig = mergeUserConfig(chainConfig, projectOptions)
 
-  romoveFile(path.resolve(`dist-${process.env.PLATFORM}/*`), error => {
-    if (error) throw error
-    webpack(finalWebpackConfig, () => {})
-  })
+  await fs.remove(path.resolve(`dist-${process.env.PLATFORM}/*`))
+  webpack(finalWebpackConfig, () => {})
 }
-
